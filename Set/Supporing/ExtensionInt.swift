defb93de21efbfe76c4559b4a2f109f11d7d57f7
Maxim Stomphorst <maximstomphorst@gmail.com>
2019-04-30 16:30:25 +0200

    arc4random int extension

---
+++ Set/Supporing/ExtensionInt.swift
@@ -0,0 +1,23 @@
+//
+//  ExtensionInt.swift
+//  Set
+//
+//  Created by Maxim Stomphorst on 30/04/2019.
+//  Copyright © 2019 Maxim Stomphorst. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+    var arc4Random: Int {
+        switch self {
+        case 1...Int.max:
+            return Int(arc4random_uniform(UInt32(self)))
+        case -Int.max..<0:
+            return Int(arc4random_uniform(UInt32(self)))
+        default:
+            return 0
+        }
+        
+    }
+}
