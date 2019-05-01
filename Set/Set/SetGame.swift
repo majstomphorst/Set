6a27324c2fb0afef6d6611347ac8ddd24a680500
Maxim Stomphorst <maximstomphorst@gmail.com>
2019-05-01 09:40:16 +0200

    Create the game engine - basic structs - setup for func isSet()

--- Set/Set/SetGame.swift
+++ Set/Set/SetGame.swift
@@ -9,5 +9,36 @@
 import Foundation
 
 struct SetGame {
+    var deckCount: Int {
+        get {
+            return deck.count;
+        }
+    }
+    
+    private var deck = SetCardDeck();
+    lazy var deckOnTable: [SetCard] = deck.draw(amount: .twelve) ?? [];
+    private(set) var beingMatched = [SetCard]();
+    private(set) var matched = [SetCard]();
+    
+    
+    
+    private typealias ProperyFunction<T:Hashable> = (SetCard) -> T;
+    
+    private static func isSetOfProperties<T:Hashable>(cards: [SetCard], function: ProperyFunction<T>) -> Bool {
+        let count = Dictionary(grouping: cards, by: function).count;
+        return count == 1 || count == cards.count;
+    }
+    public static func isSet(cards: SetCard...) -> Bool {
+        
+        return isSetOfProperties(cards: cards) { (card) -> SetCard.Number in
+            return card.number;
+            } && isSetOfProperties(cards: cards) { (card) -> SetCard.Symbol in
+                return card.symbol;
+            } && isSetOfProperties(cards: cards) { (card) -> SetCard.Shading in
+                return card.shading;
+            } && isSetOfProperties(cards: cards) { (card) -> SetCard.Color in
+                return card.color;
+        };
+    }
     
 }
