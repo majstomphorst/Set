31e93b075afad4239d25fe8df26474e7e8131593
Maxim Stomphorst <maximstomphorst@gmail.com>
2019-05-01 14:20:58 +0200

    test for IsSet function

--- SetTests/SetGameTest.swift
+++ SetTests/SetGameTest.swift
@@ -15,11 +15,36 @@ class SetGameTest: XCTestCase {
     
     func testInit() {
         var setGame = SetGame();
-        
         let deckOnTable = setGame.deckOnTable;
         
         XCTAssertFalse(deckOnTable.count == constDeck.empty.rawValue, "should not be empty");
         XCTAssertTrue(deckOnTable.count == constDeck.deckOntabelInitValue.rawValue, "twelve cards on table");
+    }
+    
+    func testIsSet() {
+        
+        // cards are a set
+        let card1 = SetCard(with: 1, 1, 1, 1);
+        let card2 = SetCard(with: 2, 1, 1, 1);
+        let card3 = SetCard(with: 3, 1, 1, 1);
+        
+        // card are a set
+        let card4 = SetCard(with: 1, 1, 3, 1);
+        let card5 = SetCard(with: 2, 1, 1, 1);
+        let card6 = SetCard(with: 3, 1, 2, 1);
+        
+        // card are not a set
+        let card7 = SetCard(with: 1, 1, 3, 1);
+        let card8 = SetCard(with: 2, 1, 1, 1);
+        let card9 = SetCard(with: 3, 1, 2, 2);
+        
+        let test1 = SetGame.isSet(cards: card1,card2,card3);
+        let test2 = SetGame.isSet(cards: card4,card5,card6);
+        let test3 = SetGame.isSet(cards: card7,card8,card9);
+        
+        XCTAssertTrue(test1,"");
+        XCTAssertTrue(test2,"");
+        XCTAssertFalse(test3,"");
     }
     
     
