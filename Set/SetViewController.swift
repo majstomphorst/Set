bdc0ee82b6e665517fcb5fe57a2f83749d73665b
Maxim Stomphorst <maximstomphorst@gmail.com>
2019-05-05 13:08:59 +0200

    made the game play ;)

--- Set/SetViewController.swift
+++ Set/SetViewController.swift
@@ -10,34 +10,6 @@ import UIKit
 
 class SetViewController: UIViewController, LayoutViews {
     
-    func updateViewFromModel() {
-        gridView.subviews.forEach {$0.removeFromSuperview() }
-        
-        var grid = Grid(layout: Grid.Layout.aspectRatio(0.7),
-                        frame: gridView.bounds.insetBy(dx: gridView.cornerRadius,
-                                                       dy: gridView.cornerRadius));
-        grid.cellCount = setGame.deckOnTable.count;
-        
-        for i in 0..<grid.cellCount {
-            if let gridItem = grid[i] {
-                let cardView = SetCardView(frame: gridItem.insetBy(dx: 4, dy: 4));
-                cardView.card = setGame.deckOnTable[i];
-                cardView.state = .NotSelected;
-                
-                let tap = UITapGestureRecognizer(target: self, action: #selector(self.cardTap(recognizer:)))
-                cardView.isUserInteractionEnabled = true;
-                cardView.addGestureRecognizer(tap);
-                
-                cardView.backgroundColor = .clear;
-                gridView.addSubview(cardView);
-            }
-        }
-    }
-    
-    @objc func cardTap(recognizer: UITapGestureRecognizer) {
-        print("tap")
-    }
-    
     var setGame = SetGame();
     
     //MARK: Outlests
@@ -54,15 +26,69 @@ class SetViewController: UIViewController, LayoutViews {
         prettifyButtons(threeMoreCardsButton, color: .darkGray);
     }
     
+    func updateViewFromModel() {
+        gridView.subviews.forEach {$0.removeFromSuperview() }
+        
+        var grid = Grid(layout: Grid.Layout.aspectRatio(0.7),
+                        frame: gridView.bounds.insetBy(dx: gridView.cornerRadius,
+                                                       dy: gridView.cornerRadius));
+        grid.cellCount = setGame.deckOnTable.count;
+        
+        for i in 0..<grid.cellCount {
+            
+            if let gridRect = grid[i] {
+                
+                let cardView = SetCardView(frame: gridRect.insetBy(dx: 4, dy: 4));
+                let setCard = setGame.deckOnTable[i];
+                cardView.card = setCard
+                cardView.state = getCardState(for: setCard);
+                
+                let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTapOnCard(_:)))
+                cardView.isUserInteractionEnabled = true;
+                cardView.addGestureRecognizer(tap);
+                
+                cardView.backgroundColor = .clear;
+                gridView.addSubview(cardView);
+            }
+        }
+    }
+    
+    private func getCardState(for card: SetCard) -> SetCardView.State {
+        
+        if (setGame.beingMatched.contains(card)) {
+            return .Selected
+        } else if (setGame.hint.contains(card)) {
+            return .Hint;
+        }
+        return .NotSelected;
+        
+    }
+    
+    @objc func handleTapOnCard(_ recognizer: UITapGestureRecognizer) {
+        let location = recognizer.location(in: gridView);
+        if let tappedView = gridView.hitTest(location, with: nil) {
+            if let cardIndex = gridView.subviews.firstIndex(of: tappedView) {
+                let card = setGame.deckOnTable[cardIndex];
+                
+                setGame.chooseCard(card: card);
+                updateViewFromModel()
+            }
+        }
+    }
+    
     //MARK: Actions
     @IBAction func touchHelp(_ sender: UIButton) {
-        
+        if let cards = setGame.findSetOnTable() {
+            setGame.hint.append(contentsOf: cards)
+            updateViewFromModel();
+        }
     }
     @IBAction func touchNewGame(_ sender: UIButton) {
         
     }
     @IBAction func touchThreeMoreCards(_ sender: UIButton) {
-        
+        setGame.dealThreeMoreCards();
+        updateViewFromModel();
     }
     
     private func prettifyButtons(_ button: UIButton, color: UIColor) {
